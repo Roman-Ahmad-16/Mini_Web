b948b0f511e7066f0685f09cc57edd3eb7f4bb0c
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2025-10-07 11:47:08 +0500

    theme

---
+++ resources/views/front/home/layouts/master.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="Invena – A modern and responsive HTML template for consulting businesses. Perfect for finance, corporate, and agency websites. SEO-friendly, fast-loading, and easy to customize. Create a professional online presence today!">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/fav.png">
+    <title>Invena Business Consulting HTML Template</title>
+    <link rel="stylesheet preload" href="{{ asset('front/assets/css/plugins/fontawesome.css') }}" as="style">
+    <link rel="stylesheet preload" href="{{ asset('front/assets/css/plugins/swiper.css') }}" as="style">
+    <link rel="stylesheet preload" href="{{ asset('front/assets/css/plugins/metismenu.css') }}" as="style">
+    <link rel="stylesheet preload" href="{{ asset('front/assets/css/plugins/magnifying-popup.css') }}" as="style">
+    <link rel="stylesheet preload" href="{{ asset('front/assets/css/plugins/odometer.css') }}" as="style">
+    <link rel="stylesheet preload" href="{{ asset('front/assets/css/vendor/bootstrap.min.css') }}" as="style">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet preload" as="style">
+    <link rel="preload" as="image" href="{{ asset('front/') }}assets/images/banner/21.webp" />
+    <link rel="stylesheet preload" href="{{ asset('front/') }}assets/css/style.css" as="style">
+</head>
+
+<body>
+
+
+
+@yield('content');
+
+
+
+<script defer src="{{ asset('front/assets/js/plugins/jquery.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/odometer.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/jquery-appear.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/gsap.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/split-text.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/scroll-trigger.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/smooth-scroll.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/metismenu.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/popup.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/vendor/bootstrap.min.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/swiper.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/plugins/contact.form.js') }}"></script>
+<script defer src="{{ asset('front/assets/js/main.js') }}"></script>
+
+
+
+</body>
+
+</html>

be869f461a3aed08291e08809ad7c22f32ae1002
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2025-10-08 11:38:29 +0500

    theme

--- resources/views/front/home/about-us.blade.php
+++ resources/views/front/home/about-us.blade.php
@@ -1,6 +1,6 @@
 @extends('front.home.layouts.master')
 @section('content')
- <!-- about us area wrapper main -->
+    <!-- about us area wrapper main -->
     <div class="rts-breadcrumb-area">
         <div class="container">
             <div class="row">
@@ -12,9 +12,9 @@
                             Smart and effective <br>
                             business agency.
                         </h1>
-                    </div>{{ asset('front/
+                    </div>
                 </div>
-            </div') }}"
+            </div>
         </div>
         <div class="shape-area">
             <img src="{{ asset('front/assets/images/about/shape/01.png') }}" alt="shape" class="one">
@@ -300,8 +300,7 @@
                                 </p>
                             </div>
                         </div>
-                        <div class="button-wrapper mt--40" data-animation="fadeInUp" data-delay="0.4"
-                            data-duration="1.2">
+                        <div class="button-wrapper mt--40" data-animation="fadeInUp" data-delay="0.4" data-duration="1.2">
                             <a href="#" class="rts-btn btn-primary">Contact Us</a>
                             <div class="vedio-icone">
                                 <a class="video-play-button play-video popup-video"
@@ -482,112 +481,7 @@
     </div>
     <!-- rts cta area end -->
 
-    <!-- footer area start -->
-    <div class="footer-8-area-bg bg_image pt--65">
-        <div class="container pb--65">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="footer-logo-area-left-8">
-                        <a href="#" class="logo">
-                            <img src="assets/images/logo/03.svg" alt="logo">
-                        </a>
-                        <p class="disc">
-                            Felis consquat magnis fames sagittis ultrices plasodales porttitor quisque ultrice tempor
-                            turpis.
-                        </p>
-                        <ul class="social-area-wrapper-two">
-                            <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                            <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                            <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="offset-lg-1 col-lg-4">
-                    <div class="footer-one-single-wized">
-                        <div class="wized-title">
-                            <h5 class="title">Quick Links</h5>
-                            <img src="assets/images/footer/under-title.png" alt="finbiz_footer">
-                        </div>
-                        <div class="quick-link-inner">
-                            <ul class="links">
-                                <li><a href="#"><i class="far fa-arrow-right"></i> Forum Support</a></li>
-                                <li><a href="#"><i class="far fa-arrow-right"></i> Help &amp; FAQ</a></li>
-                                <li><a href="#"><i class="far fa-arrow-right"></i> Contact Us</a></li>
-                                <li><a href="#"><i class="far fa-arrow-right"></i> Pricing &amp; Plans</a></li>
-                                <li><a href="#"><i class="far fa-arrow-right"></i> Cookie Policy</a></li>
-                            </ul>
-                            <ul class="links margin-left-70">
-                                <li><a href="#"><i class="far fa-arrow-right"></i> About Us</a></li>
-                                <li><a href="#"><i class="far fa-arrow-right"></i> My Account</a></li>
-                                <li><a href="#"><i class="far fa-arrow-right"></i>Our Company</a></li>
-                                <li><a href="#"><i class="far fa-arrow-right"></i>Service</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="offset-lg-1 col-lg-3">
-                    <div class="footer-one-single-wized">
-                        <div class="wized-title">
-                            <h5 class="title">Contact Us</h5>
-                            <img src="assets/images/footer/under-title.png" alt="finbiz_footer">
-                        </div>
-                        <div class="quick-link-inner d-block">
-                            <div class="signle-footer-contact-8">
-                                <div class="icon">
-                                    <i class="fa-solid fa-phone-alt"></i>
-                                </div>
-                                <div class="inner-content">
-                                    <h5 class="title">Call Us 24/7</h5>
-                                    <a href="#">(+256) 2145.2156</a>
-                                </div>
-                            </div>
-                            <div class="signle-footer-contact-8">
-                                <div class="icon">
-                                    <i class="fa-solid fa-envelope"></i>
-                                </div>
-                                <div class="inner-content">
-                                    <h5 class="title">Work with us</h5>
-                                    <a href="#">info@Invena.com</a>
-                                </div>
-                            </div>
-                            <div class="signle-footer-contact-8">
-                                <div class="icon">
-                                    <i class="fa-solid fa-location-dot"></i>
-                                </div>
-                                <div class="inner-content">
-                                    <h5 class="title">Our Location</h5>
-                                    <a href="#">XYZ Hilton Street, 125 Town <br>
-                                        United State</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="copyright-area-main-wrapper">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="copyright-8-wrapper">
-                            <p>Invena - Copyright <script>
-                                document.write(
-                                    new Date().getFullYear()
-                                )
-                            </script>. All rights reserved.</p>
-                            <ul>
-                                <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                                <li><a href="terms-of-condition.html">Terms & Condition</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
 
 
 @endsection
-

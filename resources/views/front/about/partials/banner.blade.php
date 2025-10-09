e269a891ad30e305f597b18f39a764899d4deb7f
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2025-10-09 12:14:58 +0500

    theme

---
+++ resources/views/front/about/partials/banner.blade.php
@@ -0,0 +1,14 @@
+    <!-- rts banner area start -->
+    <div class="rts-banner-area-start rts-banner-business-coach bg_image mb-5 "
+        style="height: 500px; padding: 60px 0; display: flex; align-items: center; background-size: cover; background-position: center;">
+        <div class="container text-center text-white ">
+            <div class="banner-busines-coatch-wrapper">
+
+                <h1 class="title rts-text-anime-style-1"
+                    style="font-size: 60px; line-height: 1.3; font-weight: 700; max-width: 800px; margin: 0 auto 20px;">
+                    About Us
+                </h1>
+            </div>
+        </div>
+    </div>
+    <!-- rts banner area end -->
--- resources/views/front/pages.blade.php
+++ resources/views/front/pages.blade.php
@@ -1,31 +1,7 @@
 @extends('front.home.layouts.master')
 @section('content')
 
-    <!-- rts banner area start -->
-    <div class="rts-banner-area-start rts-banner-business-coach bg_image">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="banner-busines-coatch-wrapper">
-                        <div class="pre-title">
-                            <img src="{{ asset('front/assets/images/banner/icon/04.svg') }}" alt="banner">
-                            <span>I am A Coach</span>
-                        </div>
-                        <h1 class="title rts-text-anime-style-1">Your Partner in
-                            Growth, Success,
-                            and Leadership</h1>
-                        <a href="#" class="rts-btn btn-primary btn-white">Get Your Free Guide Now</a>
 
-                        <div class="button-image-lead">
-                            <img src="{{ asset('front/assets/images/banner/small-img/01.png') }}" alt="small">
-                            <span>2k industry leaders.</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- rts banner area end -->
 
     <!-- rts service area start -->
     <div class="rts-service-area-start rts-section-gap">

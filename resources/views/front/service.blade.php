f3ff6ac75ff207ff364aaaec064c267fbe1cf229
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2025-10-10 15:31:28 +0500

    theme

--- resources/views/front/service.blade.php
+++ resources/views/front/service.blade.php
@@ -3,5 +3,42 @@
 
 @section('content')
 
+    <div class="breadcrumb-service-detals-one">
+        <div class="container-1754">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="banner-inner-service-details-1 bg_image">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="title-area-left">
+                                        <span class="bg-title">Business</span>
+                                        <h1 class="title rts-text-anime-style-1">
+                                            Our Services
+                                        </h1>
+                                        <p class="disc">
+                                            Intrinsicly coordinate multifunctional functionalities reliable
+                                            potentialities.
+                                            Objectively envisioneer high in convergence through collaborative networks.
+                                            Interactively generate B2C e-tailers for business data restore fully
+                                            researched
+                                            relationships through resource maximizing results.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    @include('front.partials.service3')
+
+    @include('front.home.partials.appointment')
+
+    @include('front.home.partials.testimonial')
 
 @endsection
